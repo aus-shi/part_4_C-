9c8201c35ca7a5ba3299e21cc4b363c3849a587f
ausdev <firdausam98@gmail.com>
2023-11-18 08:46:49 +0800

    added_casting

--- Program.cs
+++ Program.cs
@@ -48,8 +48,37 @@
 // Console.WriteLine($"int: {myInt}");
 
 // Example of narrowing conversion , meaning value become less sensitive
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
 
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+
+//Use ToString() to convert a number to a string
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// int message2 = first + second;
+// Console.WriteLine(message);
+// Console.WriteLine(message2);
+
+//Convert a string to an int using the Parse() helper method
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// Convert a string to a int using the Convert class
+// string value1 = "5";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+
+// Compare casting and converting a decimal into an int
+int value = (int)1.5m; // casting truncates
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m); // converting rounds up
+Console.WriteLine(value2);
