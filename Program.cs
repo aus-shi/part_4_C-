7d0547ae4b050f56832717eff41240624f8ffb8b
ausdev <firdausam98@gmail.com>
2023-11-20 00:34:54 +0800

    implicit-explicit-example

--- Program.cs
+++ Program.cs
@@ -119,22 +119,54 @@
 
 
 /* Exercise - Complete a challenge to combine string array values as strings and as integers */
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
-decimal total = 0m;
-string message = "";
+// decimal total = 0m;
+// string message = "";
 
-foreach (var value in values)
-{
-    decimal number; // stores the TryParse "out" value
-    if (decimal.TryParse(value, out number))
-    {
-        total += number;
-    } else
-    {
-        message += value;
-    }
-}
+// foreach (var value in values)
+// {
+//     decimal number; // stores the TryParse "out" value
+//     if (decimal.TryParse(value, out number))
+//     {
+//         total += number;
+//     } else
+//     {
+//         message += value;
+//     }
+// }
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {total}");
+
+
+/* Exercise - Complete a challenge to output math operations as specific number types */
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+// int  result1 = value1 / (int)value2;
+// Console.WriteLine($"Divide value1: {value1} by value2: {value2}, display the result as an int: {result1}");
+
+// decimal result2 = value2 / (decimal)value3;
+// Console.WriteLine($"Divide value2: {value2} by value3: {value3}  display the result as a decimal: {result2}");
+
+// float result3 = value3 / (float)value1;
+// Console.WriteLine($"Divide value3: {value3} by value1:  {value1}, display the result as a float: {result3:F7}");
+
+//Example of implicit conversion
+// int smallNumber = 5;        // This is a small number
+// double bigNumber = smallNumber;  // Implicit conversion: Small to Big
+
+// Console.WriteLine($"Small Number: {smallNumber}");
+// Console.WriteLine($"Big Number: {bigNumber}");
+
+//Example of explicit conversion
+double bigNumber = 10.75;  // This is a big number
+int smallNumber = (int)bigNumber;  // Explicit conversion: Big to Small
+
+Console.WriteLine($"Big Number: {bigNumber}");
+Console.WriteLine($"Small Number: {smallNumber}");
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c52953347bb3a073f6be9ff7ac5e102407203f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7762e65c6303f00210903f8ef970f4623645bc1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
